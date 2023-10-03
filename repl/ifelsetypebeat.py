81a36d01e9b02e5d6dda559cad1fa77b53df04b4
yerbymatey <gene@genes-MBP.attlocal.net>
2023-10-03 00:05:01 -0700

    then what

---
+++ repl/ifelsetypebeat.py
@@ -0,0 +1,16 @@
+character = input("are you a rodent? are you vaguely rodent-like?: ")
+if character == ("yes"):
+  print("""you are stuart little. you have always been stuart little. you will always be stuart little""")
+else: 
+  print("what the fuck man")
+baracter = input("""then have you got a little exoskeleton? are you a little crunchy on the outside?: """)
+if baracter == ("yes"):
+  print("wow. you're kafka's little roachy guy.")
+else:
+  print("you've disappointed me twice now.")
+daracter = input("last chance. do you have 8 arms?: ")
+if daracter == ("yes"):
+    print("waaaoowwww finally you're that spider from charlotte's web.")
+else:
+    ### i know i'm not supposed to actually come up with a failure response but i'm doing it anyway because i'm spiteful 
+  print("go fuck yourself hoss")

02047b5395a59a46b87eb0dfb3e2c7f8b38e6ee7
gene <gene@genes-MBP.attlocal.net>
2023-10-03 00:01:01 -0700

    Commit message from Replit activity

---
+++ repl/welcome.py
@@ -0,0 +1,7 @@
+print("""genie 
+october third""")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+print("i am feeling 😋 about the challenge lol") 
+print("u can follow my progress at replit.com/@yellowstripesbluespots")

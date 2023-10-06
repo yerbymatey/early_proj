cf5e0a653547dde9eb539e2d861309616a10b0b5
yerbymatey <gene@genes-MBP.attlocal.net>
2023-10-06 00:05:01 -0700

    lemme in

---
+++ repl/iguessitssecure.py
@@ -0,0 +1,11 @@
+print("it's time to log in bitch")
+username = input("what's ur username bitch")
+password = input("what's ur password hoe")
+
+if username == "genie" and password == "password":
+  print("ay welcome in bitch")
+elif username == "taco" and pasword == "password":
+  print("ay welcome in chief")
+elif username == "purveyor" and password == "password":
+  print("allo bruv!")
+else: print("get the fak out of heeeeere")

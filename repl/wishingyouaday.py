2c14b578e76069987d392e885a898339e2a05479
yerbymatey <gene@genes-MacBook-Pro.local>
2023-10-07 00:06:01 -0700

    rats on a boat

---
+++ repl/wishingyouaday.py
@@ -0,0 +1,13 @@
+print("hello friend..........")
+name = input("what's your name......")
+
+if name == "genie" or name == "Genie":
+  day = input("what's the day today...")
+  if day == "tuesday" or day == "Tuesday":
+    print("very cool, hope you have a good tuesday genie")
+  if day == "fuck off" or day == "Fuck Off":
+    print("aight then")
+elif name == "penie" or name == "Penie":
+  day = input("what's the day today")
+  if day == "nunya" or day == "Nunya":
+    print("yeesh still hope u have a good day")

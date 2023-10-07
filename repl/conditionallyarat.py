32bd8d960e116570e798aab9791773e7b2d694d3
yerbymatey <gene@genes-MacBook-Pro.local>
2023-10-07 00:05:01 -0700

    rats on a boat

---
+++ repl/conditionallyarat.py
@@ -0,0 +1,19 @@
+print("prove your loyalty..........coward ")
+movie = input("what's ur favorite movie? ")
+if movie == "flushed away":
+  print("oh......really? really? ")
+  conviction = input("then prove it lil man ")
+  if conviction == "okay":
+    print("hm...... okay")
+    character = input("what's the main character's name? ")
+    print()
+    if character == "rodney":
+      print("wow,,,, i guess you know your shit!")
+    else:
+      print("boooo i knew it u don't know ur shit")
+  else:
+    print("oh well, guess u don't care about the greatest movie of all time")
+elif movie == "shrek":
+    print("no confirmation needed. i agree.")
+else:
+  print("bleh, don't care, never heard of it.")

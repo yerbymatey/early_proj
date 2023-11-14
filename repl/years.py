d857905f439520f4bfe00f75a6a48c3a92ec4112
yerbymatey <gene@genes-MacBook-Pro.local>
2023-11-14 00:06:01 -0800

    rats on a boat

---
+++ repl/years.py
@@ -0,0 +1,8 @@
+print("this is a generation generator lol")
+year = int(input("what year were you born in"))
+if year >= 1925 and year <= 1945:
+  print("youre a traditionalist")
+elif year >= 1947 and year <= 1964 :
+  print("youre a FUCKING BOOMER")
+
+//this is all i'm doing 

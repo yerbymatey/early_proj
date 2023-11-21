e17d84341aa3698af1017df75cbe54f7cac132b9
yerbymatey <gene@genes-MacBook-Pro.local>
2023-11-21 00:10:01 -0800

    but why

---
+++ repl/song.py
@@ -0,0 +1,9 @@
+print("fill in the blank lyrics")
+print()
+while True:
+  word = input("Hey now, you're an ___ ____")
+  if word != "all star":
+    print("NOPE LOL")
+  else:
+    break
+print("nice you got it")

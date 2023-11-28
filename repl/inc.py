ff955aab4beeb035471e973e0b022b666708b62c
yerbymatey <gene@genes-MacBook-Pro.local>
2023-11-28 00:12:01 -0800

    it goes up

---
+++ repl/inc.py
@@ -0,0 +1,9 @@
+print("choose a starting number ")
+startNo = int(input( ))
+print("choose an ending number ")
+endNo = int(input( ))
+print("what do u wanna increment ")
+incNo = int(input( ))
+
+for i in range (startNo, endNo, incNo):
+    print(i)

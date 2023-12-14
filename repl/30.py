11560faec335ef5d2d4772e53796653e273a9bd0
yerbymatey <gene@genes-MacBook-Pro.local>
2023-12-14 00:16:01 -0800

    what did you think

---
+++ repl/30.py
@@ -0,0 +1,13 @@
+print("30 Days Down, BITCH")
+
+for i in range (1, 31):
+  # thoughts = input("")
+  # response = f"You thought that Day {i} was {thoughts}"
+  # print(response)
+  thought = input(f"Day {i}:\n")
+  print()
+  youThought = f"You thought Day {i} was"
+  print(f"{youThought:^35}")
+  print(f"{thought:^35}")
+  print()
+ 

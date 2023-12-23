c532e0852ca603d3f378370ca9c63e6a197feefc
yerbymatey <gene@genes-MacBook-Pro.local>
2023-12-23 00:18:01 -0800

    literally

---
+++ repl/aname.py
@@ -0,0 +1,24 @@
+print("do u want a star wars name")
+print()
+
+fName = input("what's ur first name:\n")
+lName = input("what's ur last name:\n")
+
+fSplice = fName[0:3]
+lSplice = lName[0:3]
+
+print(f"your star wars first name is {fSplice}{lSplice}")
+
+mName = input("what's u mother's maiden name:\n").lower
+city = input("what city were u born in:\n")
+
+mName = mName[0:3]
+city = city[0:3]
+
+print(f"your star wars last name is {mName}{city}")
+
+print(f"your star wars name as a whole lol is {fSplice}{lSplice} {mName}{city}")
+
+# fnlName = input("what's ur first name, last name, your mother's maiden name, and the city you were born in? pls separate each answer with a space")
+
+# print(f"{fnlName.split()}

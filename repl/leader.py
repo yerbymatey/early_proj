913fe1e40804b21059acf7284dcac5c535c5d64c
yerbymatey <gene@genes-MacBook-Pro.local>
2024-01-22 00:20:01 -0800

    open

---
+++ repl/leader.py
@@ -0,0 +1,11 @@
+#read in the data from the high.score file
+#identify which of those users had the highest scores automatically
+#output the name and score of the winner
+
+print("current leader")
+
+while True:
+  f = open("high.score", "r")
+  contents = f.readline().strip()
+  if 
+  
